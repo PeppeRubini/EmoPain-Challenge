d0792b898f3c2a252703fc46b0e503206b4b2ad7
katmiranda <kate.miranda237@gmail.com>
2023-06-12 18:49:43 +0200

    Cambiati i path e inserita la funzione in cleaning.py, aggiunto il dataset pulito

--- cleaning.py
+++ cleaning.py
@@ -8,49 +8,28 @@ header_list = ['frame', ' face_id', ' timestamp', ' confidence', ' AU01_r', ' AU
                ' AU45_r', ' AU01_c', ' AU02_c', ' AU04_c', ' AU05_c', ' AU06_c', ' AU07_c', ' AU09_c', ' AU10_c',
                ' AU12_c',
                ' AU14_c', ' AU15_c', ' AU17_c', ' AU20_c', ' AU23_c', ' AU25_c', ' AU26_c', ' AU28_c', ' AU45_c']
+
 label_data_path_train = "./Dataset/Pain Labels/train/"
 geometric_data_path_train = "./Dataset/Face Features/Geometric Features/train/"
-# label_data_path_valid = "./Dataset/Pain Labels/valid/"
-# geometric_data_path_valid = "./Dataset/Face Features/Geometric Features/valid/"
+label_data_path_valid = "./Dataset/Pain Labels/valid/"
+geometric_data_path_valid = "./Dataset/Face Features/Geometric Features/valid/"
 
-# todo rendere questa una funzione a cui si passa il path delle pain lable e delle feature geometriche
-#  in modo da riutilizzarla per train e validation set
-for file in os.listdir(label_data_path_train):
-    # metto insieme le due tabelle
-    df_geo = pd.read_csv(geometric_data_path_train + file)
-    df_label = pd.read_csv(label_data_path_train + file, header=None)
-    df_label.columns = ['label']
-    df_merged = pd.concat([df_geo, df_label], axis=1)
-    # filtro le righe inutili
-    df_merged_clean = df_merged[(df_merged[' success'] != 0) & (df_merged['label'] > 0)]
-    # salvo il file delle pain lable todo cambiare percorso
-    df_label_clean = df_merged_clean.pop('label')
-    df_label_clean.to_csv(file, index=False, header=None)
-    # salvo il file delle feature todo cambiare percorso
-    df_geo_clean = df_merged_clean[header_list]
-    df_geo_clean.to_csv(file, index=False)
+def clean(label_path, geometric_path):
+    for file in os.listdir(label_path):
+        # metto insieme le due tabelle
+        df_geo = pd.read_csv(geometric_path + file)
+        df_label = pd.read_csv(label_path + file, header=None)
+        df_label.columns = ['label']
+        df_merged = pd.concat([df_geo, df_label], axis=1)
+        # filtro le righe inutili
+        df_merged_clean = df_merged[(df_merged[' success'] != 0) & (df_merged['label'] > 0)]
+        if not df_merged_clean.empty:
+            # salvo il file delle pain lable
+            df_label_clean = df_merged_clean.pop('label')
+            df_label_clean.to_csv(str(label_path).replace("Dataset", "CleanDataset") + file, index=False, header=None)
+            # salvo il file delle feature
+            df_geo_clean = df_merged_clean[header_list]
+            df_geo_clean.to_csv(str(geometric_path).replace("Dataset", "CleanDataset") + file, index=False)
 
-"""    # elimino dati con success = 0
-    df_geo_clean = df_geo[df_geo[' success'] != 0]
-    # considero solo le colonne interessate
-    df_geo_clean = df_geo_clean[header_list]
-    # salvo todo creare le directory per il dataset pulito
-    df_geo_clean.to_csv("ripulito.csv", index=False)"""
-"""    df_label = pd.read_csv(label_data_path_train + file, header=None)  # header = None perchè non c'è intestazione
-    i = 0
-    for k, v in df_label.items():  # v è una series
-        for val in v:
-            if val > 0:
-                print(val, file, i)  # creare file
-            i += 1"""
-
-"""
-for label_value in df_label:
-    print(label_value)
-    if int(label_value) > 0:
-        #crea file
-        print("c'è dolore")
-        """
-# print(csv_label)
-# csv_geo = pd.read_csv(geometric_data_path_train + file)
-# print(csv_geo)
+clean(label_data_path_train, geometric_data_path_train)
+clean(label_data_path_valid, geometric_data_path_valid)
