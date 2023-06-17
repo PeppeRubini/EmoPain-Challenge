979858e7f9b3223d8ab819db71bfe6adf330ef7b
PeppeRubini <giusepperubini5@gmail.com>
2023-06-17 12:54:17 +0200

    nella pulizia teniamo solo le action unit e creata LSTM

--- training.py
+++ training.py
@@ -1,14 +1,10 @@
-from tensorflow import keras
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPool2D, AvgPool2D, Dense, LSTM
-import os
-import pandas as pd
 import numpy as np
-
-path_geo_train = "./CleanDataset/Face Features/Geometric Features/train/"
-path_label_train = "./CleanDataset/Pain Labels/train/"
-path_geo_valid = "./CleanDataset/Face Features/Geometric Features/valid/"
-path_label_valid = "./CleanDataset/Pain Labels/valid/"
+import pandas as pd
+import os
+import keras
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
 
 
 def csv_to_array(path_geo, path_label):
@@ -24,43 +20,50 @@ def csv_to_array(path_geo, path_label):
     return arr_geo, arr_label
 
 
+def array_input_step(x, step):
+    X = []
+    i = 0
+    for e in x[:-(step-1)]:
+        l = []
+        l.append(list(e))
+        i += 1
+        for j in range(i,step + i - 1):
+            l.append(list(x[j]))
+        X.append(l)
+    return np.array(X)
+
+
+def array_output_step(y, step):
+    Y = y[step-1:]
+    return np.array(Y)
+
+
+path_geo_train = "./CleanDataset/Face Features/Geometric Features/train/"
+path_label_train = "./CleanDataset/Pain Labels/train/"
+path_geo_valid = "./CleanDataset/Face Features/Geometric Features/valid/"
+path_label_valid = "./CleanDataset/Pain Labels/valid/"
+time_step = 3
+num_features = 17
 x_train, y_train = csv_to_array(path_geo_train, path_label_train)
 x_valid, y_valid = csv_to_array(path_geo_valid, path_label_valid)
-
-"""print(x_valid.shape)
-print(y_valid.shape)
-print(x_train[0])
-print(x_train[x_train.shape[0]-1])
-print("............................")
-print(y_train[0])
-print(y_train[y_train.shape[0]-1])
-print("-----------------------")
-print(x_valid[0])
-print(x_valid[x_valid.shape[0]-1])
-print("............................")
-print(y_valid[0])
-print(y_valid[y_valid.shape[0]-1])"""
+X_t = array_input_step(x_train, time_step)
+Y_t = array_output_step(y_train, time_step)
+X_v = array_input_step(x_valid, time_step)
+Y_v = array_output_step(y_valid, time_step)
 
 model = Sequential()
-# model.add(Conv2D(units=64, activation='relu'))
-# model.add(MaxPool2D(units=32))
-"""model.add(LSTM(units=64, activation='relu'))
-model.add(LSTM(units=32, activation='relu'))
-model.compile(loss='binary_crossentropy',
-              optimizer='sgd',
-              metrics=['accuracy'])"""
-time_steps = 1
-num_features = 39
-x_train = x_train.reshape(x_train.shape[0], time_steps, num_features)
 model.add(LSTM(64, dropout=0.5, recurrent_dropout=0.2,
-               input_shape=(time_steps, num_features), return_sequences=True))
+               input_shape=(time_step, num_features), return_sequences=True))
 model.add(LSTM(64, dropout=0.5, recurrent_dropout=0.2))
+model.add(Dense(64))
+model.add(Dense(32))
 model.add(Dense(1))
 
-"""model.add(Dense(64, activation="relu"))
-model.add(Dense(32, activation="relu"))
-model.add(Dense(1, activation="sigmoid"))"""
+model.compile(optimizer="adam", loss="mse", metrics=[tf.keras.metrics.RootMeanSquaredError(), tf.keras.metrics.MeanAbsoluteError()])
 
-model.compile(optimizer="adam", loss="mse", metrics="accuracy")
-model.fit(x_train, y_train, epochs=5, batch_size=32)
-loss_and_metrics = model.evaluate(x_valid, y_valid, batch_size=128)
+model.fit(X_t, Y_t, epochs=10)
+
+X_v = X_v.reshape((len(X_v), time_step, num_features))
+
+y_test = model.predict(X_v)
+print(y_test)
