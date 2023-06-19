f47757d31fcda5a1df92652d49fa5602a579b8e6
katmiranda <kate.miranda237@gmail.com>
2023-06-19 12:18:04 +0200

    modificato cleaning.py per includere solamente esempi di dolore cronico; modificato il dataset pulito

--- cleaning.py
+++ cleaning.py
@@ -12,21 +12,19 @@ geometric_data_path_valid = "./Dataset/Face Features/Geometric Features/valid/"
 
 def clean(label_path, geometric_path):
     for file in os.listdir(label_path):
-        # metto insieme le due tabelle
-        df_geo = pd.read_csv(geometric_path + file)
-        df_label = pd.read_csv(label_path + file, header=None)
-        df_label.columns = ['label']
-        df_merged = pd.concat([df_geo, df_label], axis=1)
+        if (file.startswith("P")):
+            # metto insieme le due tabelle
+            df_geo = pd.read_csv(geometric_path + file)
+            df_label = pd.read_csv(label_path + file, header=None)
+            df_label.columns = ['label']
+            df_merged = pd.concat([df_geo, df_label], axis=1)
 
-        # filtro le righe inutili
-        df_merged_clean = df_merged[(df_merged[' success'] != 0) & (df_merged['label'] > 0)]
-        if not df_merged_clean.empty:
             # salvo il file delle pain lable
-            df_label_clean = df_merged_clean.pop('label')
-            df_label_clean.to_csv(str(label_path).replace("Dataset", "CleanDataset") + file, index=False, header=None)
+            df_label = df_merged.pop('label')
+            df_label.to_csv(str(label_path).replace("Dataset", "CleanDataset") + file, index=False, header=None)
             # salvo il file delle feature
-            df_geo_clean = df_merged_clean[header_list]
-            df_geo_clean.to_csv(str(geometric_path).replace("Dataset", "CleanDataset") + file, index=False)
+            df_geo = df_merged[header_list]
+            df_geo.to_csv(str(geometric_path).replace("Dataset", "CleanDataset") + file, index=False)
 
 
 clean(label_data_path_train, geometric_data_path_train)
